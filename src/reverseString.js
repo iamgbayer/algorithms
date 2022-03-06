b73531ed36aae1477bca4b96c069bd78b9dfe9d2
Guilherme Bayer <iamgbayer@gmail.com>
2022-03-06 14:19:14 -0300

    feat: adding second version of reverse string and removing unnecessary file

--- src/reverseString.js
+++ src/reverseString.js
@@ -3,3 +3,13 @@ export const reverseString = (string) =>
     .split(" ")
     .map((word) => word.split("").reverse().join(""))
     .join(" ");
+
+export const reverseStringThroughLoop = (string) => {
+  let reversed = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i];
+  }
+
+  return reversed;
+};
--- src/reverseString.test.js
+++ src/reverseString.test.js
@@ -1,6 +1,11 @@
-import { reverseString } from "./reverseString";
+import { reverseString, reverseStringThroughLoop } from "./reverseString";
 
 it("Should reverse each word correctly", () => {
   expect(reverseString("my name is guilherme")).toEqual("ym eman si emrehliug");
   expect(reverseString("guilherme")).toEqual("emrehliug");
+
+  expect(reverseStringThroughLoop("her name is bia")).toEqual(
+    "aib si eman reh"
+  );
+  expect(reverseStringThroughLoop("type  script")).toEqual("tpircs  epyt");
 });
